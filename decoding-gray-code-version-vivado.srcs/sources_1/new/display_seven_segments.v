62298ee236eb7b4b32015e046a228f6c733491d3
Carlos12001 <62664256+Carlos12001@users.noreply.github.com>
2022-10-04 12:49:40 -0600

    Vivado directory project

---
+++ decoding-gray-code-version-vivado.srcs/sources_1/new/display_seven_segments.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/04/2022 12:45:58 PM
+// Design Name: 
+// Module Name: display_seven_segments
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module display_seven_segments(
+
+    );
+endmodule
