442419d4dec4bc268acb36cd9e7aa459c2b02614
AndresRB506 <andresrb506@gmail.com>
2022-10-07 16:32:54 -0600

    iniciando branch flipflop

--- decoding-gray-code-version-vivado.runs/synth_1/display_seven_segments.tcl
+++ decoding-gray-code-version-vivado.runs/synth_1/display_seven_segments.tcl
@@ -17,19 +17,18 @@ proc create_report { reportName command } {
     send_msg_id runtcl-5 warning "$msg"
   }
 }
-set_param chipscope.maxJobs 2
 create_project -in_memory -part xc7a35tcpg236-1
 
 set_param project.singleFileAddWarning.threshold 0
 set_param project.compositeFile.enableAutoGeneration 0
 set_param synth.vivado.isSynthRun true
-set_property webtalk.parent_dir C:/Users/carlo/Desktop/random/decoding-gray-code-version-vivado/decoding-gray-code-version-vivado.cache/wt [current_project]
-set_property parent.project_path C:/Users/carlo/Desktop/random/decoding-gray-code-version-vivado/decoding-gray-code-version-vivado.xpr [current_project]
+set_property webtalk.parent_dir {C:/Users/ikari/OneDrive/Escritorio/Proyecto 2 DisLog/decoding-gray-code-version-vivado/decoding-gray-code-version-vivado.cache/wt} [current_project]
+set_property parent.project_path {C:/Users/ikari/OneDrive/Escritorio/Proyecto 2 DisLog/decoding-gray-code-version-vivado/decoding-gray-code-version-vivado.xpr} [current_project]
 set_property default_lib xil_defaultlib [current_project]
 set_property target_language Verilog [current_project]
-set_property ip_output_repo c:/Users/carlo/Desktop/random/decoding-gray-code-version-vivado/decoding-gray-code-version-vivado.cache/ip [current_project]
+set_property ip_output_repo {c:/Users/ikari/OneDrive/Escritorio/Proyecto 2 DisLog/decoding-gray-code-version-vivado/decoding-gray-code-version-vivado.cache/ip} [current_project]
 set_property ip_cache_permissions {read write} [current_project]
-read_verilog -library xil_defaultlib C:/Users/carlo/Desktop/random/decoding-gray-code-version-vivado/decoding-gray-code-version-vivado.srcs/sources_1/new/display_seven_segments.v
+read_verilog -library xil_defaultlib {{C:/Users/ikari/OneDrive/Escritorio/Proyecto 2 DisLog/decoding-gray-code-version-vivado/decoding-gray-code-version-vivado.srcs/sources_1/new/display_seven_segments.v}}
 # Mark all dcp files as not used in implementation to prevent them from being
 # stitched into the results of this synthesis run. Any black boxes in the
 # design are intentionally left as such for best results. Dcp files will be
